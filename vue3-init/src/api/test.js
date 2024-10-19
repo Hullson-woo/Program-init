b2b1e34ae1347047904b86e91c0218a4182dd02e
Hullson <hullsonwoo@gmail.com>
2024-10-19 15:17:32 +0800

    Delete vue3-init directory

--- vue3-init/src/api/test.js
+++
@@ -1,9 +0,0 @@
-import request from '@/request/index.js'
-const url = `${import.meta.env.VITE_APP_BASE_URL}/advisor`
-
-export function testApi() {
-  return request({
-    url: `${url}/test`,
-    method: 'get',
-  })
-}
